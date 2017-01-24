682c6bb6ab32594c09d576cb1ffe01b3ab344cf2
Joseph Orbegoso Pea <joe@trusktr.io>
2017-01-23 21:23:52 -0800

    Remove completely irrelevant comments.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,4 @@
-//import './setup-globals'
 import Foo from './Foo'
-//import './Bar'
-
-// Prove we can import IMVU code in `module()` format:
-//import cps from 'imvu/cps'
-//console.log(' -- imported IMVU module:\n', cps)
-//cps.sequence_([()=>{}], ()=>{}) // <-- no type errors.
 
 const sleep = time => new Promise(r => setTimeout(r, time))
 
